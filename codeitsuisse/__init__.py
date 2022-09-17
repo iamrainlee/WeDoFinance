4fca5dc4dbe4b7e3fea108852993a708da242dad
Lee Sze Choi <iamrainlee@gmail.com>
2022-09-17 10:24:58 +0800

    add init

--- codeitsuisse/__init__.py
+++ codeitsuisse/__init__.py
@@ -3,6 +3,7 @@ app = Flask(__name__)
 import codeitsuisse.routes.square
 import codeitsuisse.routes.tickerStreamPart1
 import codeitsuisse.routes.tickerStreamPart2
+import codeitsuisse.routes.rubiks
 
 
 
