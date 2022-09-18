77767b56618b2cf3e428a777c8e3fe9591159bfd
Lee Sze Choi <iamrainlee@gmail.com>
2022-09-18 09:49:56 +0800

    add answer.bin

--- codeitsuisse/routes/payload_shellcode.py
+++ codeitsuisse/routes/payload_shellcode.py
@@ -12,6 +12,6 @@ logger = logging.getLogger(__name__)
 
 @app.route('/payload_shellcode', methods=['GET'])
 def payload_shellcode():
-    with open('/app/codeitsuisse/routes/payload_shellcode', mode='rb') as file: # b is important -> binary
+    with open('/app/codeitsuisse/routes/answer.bin', mode='rb') as file: # b is important -> binary
         fileContent = file.read()
         return fileContent
